a1ed8a3926b65bf138a0c162d48a39a97750393a
Abdul Musavir <abdulmusavirc@gmail.com>
2022-08-16 19:52:08 +0530

    capitalize , center , casefold added

---
+++ pythonBuiltinFunction.py
@@ -0,0 +1,13 @@
+
+# ################   STRING FUNCTIONS   ################
+
+# converts first character to uppercase and others to lowercase
+sentence = "i love PYTHON"
+capitalized_string = sentence.capitalize()
+print(capitalized_string)
+
+# returns the centered padded string of length 25
+sentence = "Python is awesome"
+new_string = sentence.center(25, '*')
+
+print(new_string)
