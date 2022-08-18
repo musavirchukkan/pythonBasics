832c93ea0e36cdc397db068e389a10fbfced0e22
Abdul Musavir <abdulmusavirc@gmail.com>
2022-08-18 12:30:08 +0530

    string finished

--- pythonBuiltinFunction.py
+++ pythonBuiltinFunction.py
@@ -6,41 +6,50 @@ sentence = "i love PYTHON"
 capitalized_string = sentence.capitalize()
 print(capitalized_string)
 
+
 # returns the centered padded string of length 25
 sentence = "Python is awesome"
 new_string = sentence.center(25, '*')
 print(new_string)
+
 
 # convert all characters to lowercase
 text = "pYtHon"
 lowercased_string = text.casefold()
 print(lowercased_string)
 
+
 # number of occurrence of 'p'
 message = 'python is popular programming language'
 print('Number of occurrence of p:', message.count('p'))
 
+
 # check if the message ends with fun
 message = 'Python is fun'
 print(message.endswith('fun'))
+
 
 # '\t' replaced with whitespace characters
 str = 'xyz\t12345\tabc'
 result = str.expandtabs()
 print(result)
 
+
 # The encode() method returns an encoded version of the given string.
 title = 'Python Programming'
 print(title.encode())
+
 
 # check the index of 'fun'
 message = 'Python is a fun programming language'
 print(message.find('fun'))
 
+
 # find the index of is
 text = 'Python is fun'
 result = text.index('is')
 print(result)
+
 
 # string contains either alphabet or number
 name1 = "Python3"
@@ -58,9 +67,11 @@ print(name1.isalnum())
 # string contains printable characters ( string.isprintable())
 # string contains titlecase ( string.istitle())
 
+
 # join elements of text with space
 text = ['Python', 'is', 'a', 'fun', 'programming', 'language']
 print(' '.join(text))
+
 
 # The string ljust() method returns a left-justified string of a given minimum width.
 string = 'cat'
@@ -70,9 +81,9 @@ fillchar = '*'
 print(string.ljust(width))
 # print left justified string and fill the remaining spaces
 print(string.ljust(width, fillchar))
-
 # The string rjust() method returns a right-justified string of a given minimum width.
 print(string.rjust(width, fillchar))
+
 
 # convert message to lowercase
 message = 'PYTHON IS FUN'
@@ -85,3 +96,27 @@ print(message.upper())
 # converts lowercase to uppercase and vice versa
 name = "JoHn CeNa"
 print(name.swapcase())
+
+
+# remove leading and trailing whitespaces
+message = '     Learn Python  '
+print('Message:', message.strip())
+# remove trailing whitespace from title (message.rstrip())
+# remove leading whitespace from title (message.lstrip())
+
+
+# method splits the string at the first occurrence of the argument string and returns a tuple containing the part the before separator, argument string and the part after the separator
+string = "Python is fun"
+# 'is' separator is found
+print(string.partition('is '))
+
+
+# replace Characters
+text = 'bat ball'
+replaced_text = text.replace('b', 'c')
+print(replaced_text)
+
+
+# split the text from space
+text = 'Python is a fun programming language'
+print(text.split(' '))
